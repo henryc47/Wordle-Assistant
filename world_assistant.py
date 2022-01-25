6eff0fda0e469d81f70396ad89ba30f860c537cb
Henry Chadban <HenryJ.Chadban@Gmail.com>
2022-01-25 21:48:30 +1100

    added functionality to rank possible words by the frequency of letters and letter-position combinations in the list of allowed words, high ranked words will be more effective at narrowing down the list of possible words in the next round

--- world_assistant.py
+++ world_assistant.py
@@ -4,6 +4,8 @@
 
 #our source for english words was compiled by John Lawler of the University of Michigan
 #it is available at http://www-personal.umich.edu/~jlawler/wordlist.html
+import numpy as np#for the argsort
+
 
 text_list = "/Users/henry_chadban/Documents/Personal Projects, Current/Wordle Assistant/wordlist.txt"
 
@@ -108,6 +110,33 @@ def wordle_help():
             num_words = len(allowed_words)
             print("number of words allowed now ", num_words, " out of ", num_words_original, " five letter words")
         
+        elif(user_input=="get letter frequencies") or (user_input=='glf'):
+            letter_frequencies = get_letter_frequency(allowed_words)
+            for i in range(26):#go through all the letters in the alphabet
+                print(chr(i+97),letter_frequencies[i])
+                
+        elif(user_input=="get position frequencies") or (user_input=='gpf'):
+            position_frequencies =  get_letter_position_frequency(allowed_words)
+            for i in range(26):#go through all the letters in the alphabet
+                print(chr(i+97),end=' ')
+                for j in range(5):#go through all the characters in each five letter word
+                    letter_position = i*5 + j
+                    position_frequency = position_frequencies[letter_position]
+                    print(position_frequency,end=' ')
+                print('')
+        
+        elif(user_input=='word value') or (user_input=='wv'):
+            word_values = word_value(allowed_words)
+            for i in range(len(allowed_words)):
+                print(allowed_words[i],' ',word_values[i])
+            continue
+        
+        elif(user_input=='best words' or (user_input=='bw')):
+            user_input2 =  int(input("how many words do you want to print: "))
+            best_n_words(user_input2,allowed_words)
+            continue
+            
+        
         elif(user_input=="end"):
             return 0
         
@@ -178,7 +207,77 @@ def ban_multiple_letters(letter,max_num,allowed_words):
             continue
         
     return new_words
+
+#gets the frequency of letters in the list of allowed words
+def get_letter_frequency(allowed_words):
+    letter_frequencies = [0]*26
+    #letter frequencies stored as a length 26 list
+    for word in allowed_words:
+        for i in word:
+            letter_value = ord(i)-97
+            if (letter_value>=0) and (letter_value<=26):
+                letter_frequencies[letter_value] = letter_frequencies[letter_value]+1
+            else:
+                continue
+
+    return letter_frequencies         
                 
+def get_letter_position_frequency(allowed_words):
+    letter_position_frequencies = [0]*26*5
+    #array position is determined by letter position in alphabet times five + position in word starting at zero
+    for word in allowed_words:
+        for i in range(5):
+            letter = word[i]
+            letter_value = ord(letter)-97
+            if (letter_value>=0) and (letter_value<=26):
+                total_value  = (letter_value*5)+i
+                letter_position_frequencies[total_value] = letter_position_frequencies[total_value]+1
+            else:
+                continue
+            
+    return letter_position_frequencies          
+
+def word_value(allowed_words):
+    letter_frequency = get_letter_frequency(allowed_words)
+    position_frequency = get_letter_position_frequency(allowed_words)
+    num_words = len(allowed_words)
+    word_value_array = [0]*num_words#array to store the value of every word
+    for i in range(num_words):
+        word = allowed_words[i]
+        letter_in_word = [0]*26#is a letter in the word
+        for j in range(5):#go through all the characters in a word
+            letter = word[j]
+            letter_value = ord(letter)-97#value of the letter where a=0,b=1,etc
+            if(letter_value>=0) and (letter_value<=26):#make sure it is a valid lower case letter
+                letter_position_value = letter_value*5+j
+                #add the letter position frequency to the word value
+                word_value_array[i] = word_value_array[i]+position_frequency[letter_position_value]
+                #we know the letter is now in the word
+                letter_in_word[letter_value]=1
+            else:
+                continue
+        #add the overall frequency of all the letters found in the word to the word value
+        #we only do not double count letters found in a word more than once
+        value_from_letters = 0
+        for k in range(26):
+            value_from_letters = value_from_letters + letter_frequency[k]*letter_in_word[k]
+        
+        word_value_array[i] = word_value_array[i]+value_from_letters
+        
+    return word_value_array        
+                      
+def best_words(allowed_words):
+    word_values = word_value(allowed_words)
+    word_values = np.array(word_values)
+    word_ranks = word_values.argsort()
+    word_ranks = word_ranks[::-1]#reverse the array so largest values are at the left side
+    return (word_ranks,word_values)
+
+def best_n_words(n,allowed_words):
+    (word_ranks,word_values) = best_words(allowed_words)
+    number_words = len(allowed_words)
+    for i in range(min(n,number_words)):
+        print(i, ' ', allowed_words[word_ranks[i]],' ',word_values[word_ranks[i]])
+    return
 
         
-
