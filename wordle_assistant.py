f98b7d24eae0077b42eb2374bcbfeb6cd8c179dc
Henry Chadban <HenryJ.Chadban@Gmail.com>
2022-02-07 18:26:00 +1100

    added autoplay functionality to allow an automated strategy, contained inside a 'player class' to play the game by itself, also created a simple random strategy to test this out

--- wordle_assistant.py
+++ wordle_assistant.py
@@ -8,12 +8,14 @@
 import numpy as np#for the argsort
 import random
 
-text_list = "/Users/henry_chadban/Documents/Personal Projects, Current/Wordle Assistant/all_words.txt"
+#list of words that the wordle program accepts as valid
+allowed_list = "/Users/henry_chadban/Documents/Personal Projects, Current/Wordle Assistant/all_words.txt"
+#list of words that the wordle program can use as the secret word
 possible_list = "/Users/henry_chadban/Documents/Personal Projects, Current/Wordle Assistant/possible_words.txt"
 
 
-def import_words():
-    with open(text_list) as f:
+def import_words(source):
+    with open(source) as f:
         lines = f.readlines()
     
     culled_lines = []
@@ -23,22 +25,76 @@ def import_words():
 
     return culled_lines
 
+
+
 #wrapper for wordle_autoplay to automatically test a strategy on all wordle words
 def wordle_autoplay_all(strategy,game_type): 
     pass    
+
+
+#serves as the base class for different "player classes" which implement different 
+#strategies for playing wordle 
+class wordle_player_base():
+    #provide the player with initial data
+    def __init__(self,verbose):
+        self.allowed_words = restrict_word_length(import_words(allowed_list),5)
+        self.possible_words = restrict_word_length(import_words(possible_list),5)
+        self.verbose = verbose#if verbose == "true", every response will be printed
+        self.new_response = "z"
+    #which word will the AI provide next, must be overriden by child class
+    def response(self,feedback):
+        raise NotImplementedError("Must override response in derived class")
+        return "error"
+
+#randomly chooses a word from the list of still possible allowed words
+class wp_random_allowed(wordle_player_base):
+    def response(self,feedback):
+        self.allowed_words = restrict_list(self.allowed_words,self.new_response,feedback)
+        num_words = len(self.allowed_words)
+        random_word_position = random.randint(0,num_words-1)
+        self.new_response = self.allowed_words[random_word_position]
+        return(self.new_response)
     
-
+#create a player with a particular strategy and level of verbosity
+def create_player(verbosity,strategy):
+    if(strategy=='random_allowed' or strategy=='ra'):
+        player = wp_random_allowed(verbosity)
+    else:
+        raise NotImplementedError("There is no ", strategy, " class implemented")
     
-
-
-
-
-
+    return player
+    
+        
+        
 #autoplays wordle when given a single word
-def wordle_autoplay(strategy,word):
+def wordle_autoplay(verbosity,strategy,word):
     #allowed strategies are best word , random and worst word
-    pass
+    player = create_player(verbosity,strategy)
+    info = "start"
+    turns = 0
+    #loop till we guess the correct word
+    while(info!=list("ggggg")):
+        guess = player.response(info)
+        info = feedback(guess,word)
+        turns = turns + 1
+        if(verbosity=='true'):
+            print(turns, " ", guess, " ", info)
     
+    if(verbosity=='true' or verbosity=='true_shallow'):
+        print(strategy," deduced the word was ", word, " in ",turns, " turns")
+    return turns
+
+        
+    
+    
+    
+    
+
+
+
+
+
+
 #when provided with a "key" word and a "pass" word, the feedback engine
 #will provide a string indicating how correct the "key" word is
 #g for "green" means correct letter and position, y for "yellow" means letter is in the word
@@ -87,9 +143,49 @@ def feedback(keyword,password):
                 result[i] = 'y'
             else:
                 result[i] = 'b'
-    
-    return result        
+    return result
+        
             
+#restricts a word list in response to feedback
+def restrict_list(word_list,word,feedback):
+    if(feedback=="start"):#if this is the starting round
+        pass
+    else:
+        
+        #create an array of letter frequency to help with banning multiple letters
+        dummy = []
+        dummy.append(0)
+        letter_frequencies_word = dummy*26
+        for letter in word:
+            position = ord(letter)-97
+            letter_frequencies_word[position] = letter_frequencies_word[position]+1
+        
+        #then proceed to restrict the word list
+        for i in range(len(word)):
+            letter = word[i]
+            letter_feedback = feedback[i]
+            if(letter_feedback=='g'):
+                word_list = force_letter_position(letter,i+1,word_list)
+            if(letter_feedback=='y'):
+                word_list = force_letter(letter, word_list)
+                word_list = ban_letter_position(letter, i+1 , word_list)
+            if(letter_feedback=='b'):
+                position = ord(letter)-97
+                letter_frequency = letter_frequencies_word[position]
+                if(letter_frequency==1):
+                    #if only one copy of the letter in the word, black indicates we can just ban it
+                    #from the list of possible words
+                    word_list = ban_letter(letter,word_list)
+                elif(letter_frequency>1):
+                    #if multiple copies of the letter in the word, we know maximum number of copies
+                    #of that letter in the word are at least one less than the current number of copies
+                    word_list = ban_multiple_letters(letter,letter_frequency-1,word_list)
+                    #if maximum number of copies of that letter are even less, there will be more black
+                    #feedback in the future
+                    letter_frequencies_word[position] = letter_frequencies_word[position]-1
+        
+    return word_list
+        
             
             
             
@@ -108,7 +204,7 @@ def restrict_word_length(list_words,length):
 
 
 def wordle_help():
-    all_words = import_words()
+    all_words = import_words(allowed_list)
     #we are only interested in five letter words
     all_words = restrict_word_length(all_words,5)
     allowed_words = all_words
@@ -256,6 +352,10 @@ def wordle_help():
         else:
             print("please enter valid input")
             continue
+
+
+
+
             
 def ban_letter(letter,allowed_words):
     new_words = []
